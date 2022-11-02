2d4e4c8a07a3ec7590346930d9af452d73728cfb
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-11-02 15:50:51 -0400

    first commit

---
+++ projets/Annexe_1_Shared_Preferences/app/src/main/java/com/example/annexe_1_shared_preferences/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.annexe_1_shared_preferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
