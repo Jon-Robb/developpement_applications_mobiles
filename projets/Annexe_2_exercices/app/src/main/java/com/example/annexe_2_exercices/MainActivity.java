8c97fccc6048e34a0ec644e8ab8901cff28893f3
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-11-03 15:53:49 -0400

    premier commit

---
+++ projets/Annexe_2_exercices/app/src/main/java/com/example/annexe_2_exercices/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.annexe_2_exercices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
