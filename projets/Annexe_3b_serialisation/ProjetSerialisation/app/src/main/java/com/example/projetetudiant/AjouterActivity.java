9a4c788c2a117d6f4530154701a8d3058cbb0d5b
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-11-09 17:03:11 -0500

    ça marche

---
+++ projets/Annexe_3b_serialisation/ProjetSerialisation/app/src/main/java/com/example/projetetudiant/AjouterActivity.java
@@ -0,0 +1,46 @@
+package com.example.projetetudiant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AjouterActivity extends AppCompatActivity {
+
+    Button btnRetour;
+    EditText champAjouterPensee;
+    Ecouteur ec;
+    ListePensees instance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter);
+
+        btnRetour = findViewById(R.id.btnRetour);
+        champAjouterPensee = findViewById(R.id.champAjouterPensee);
+        ec = new Ecouteur();
+        btnRetour.setOnClickListener(ec);
+        instance = ListePensees.getInstance(AjouterActivity.this);
+    }
+
+    private class Ecouteur implements View
+
+            .OnClickListener{
+
+        @Override
+        public void onClick(View source) {
+            if (!champAjouterPensee.equals("")){
+
+                instance.ajouterPensee(champAjouterPensee.getText().toString());
+                Intent retour = new Intent();
+                retour.putExtra("texte", "Pensée ajoutée!");
+                setResult(24, retour);
+            }
+            finish();
+        }
+    }
+}
